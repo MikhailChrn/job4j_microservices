be7e085d1047835b77a42d8f85c55a037be8ed86
mikhail <mikhail.cherneyev@yandex.ru>
2026-03-20 18:13:31 +0400

    feat: add Maven project skeleton for service-r and service-s
    Create empty Spring-Boot-3 (Java-21) projects with basic dependencies, main classes, and application.yml stubs. Add init-data/ placeholder.

---
+++ service-r/src/main/java/ru/job4j/servicer/ServiceRApplication.java
@@ -0,0 +1,12 @@
+package ru.job4j.servicer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRApplication.class, args);
+    }
+}
---
+++ service-s/src/main/java/ru/job4j/services/ServiceSApplication.java
@@ -0,0 +1,12 @@
+package ru.job4j.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSApplication.class, args);
+    }
+}
